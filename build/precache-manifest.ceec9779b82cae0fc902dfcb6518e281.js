bb5420f2b8964ddd94eac12eeee17ca974d1217e
raystatic <rahul9650ray@gmail.com>
2020-09-23 03:48:25 +0530

    first commit

---
+++ build/precache-manifest.ceec9779b82cae0fc902dfcb6518e281.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3915516b48cc7a8ec48ae3ee2de59066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e26560c2c3b788bf060",
+    "url": "/static/css/main.362bdc4c.chunk.css"
+  },
+  {
+    "revision": "bba6a043763cbf6ee2cd",
+    "url": "/static/js/2.47760279.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.47760279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e26560c2c3b788bf060",
+    "url": "/static/js/main.5384968b.chunk.js"
+  },
+  {
+    "revision": "badbf884cee500e91b60",
+    "url": "/static/js/runtime-main.2cbda344.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import TinderCard from 'react-tinder-card';
+import './App.css';
+import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
+
+function App() {
+  return (
+    <div className="app">
+      <Header/>
+      <TinderCards/>
+      <SwipeButtons/>
+    </div>
+  );
+}
+
+export default App;
